05293262514eb830430ba452a07c26e6d2af5809
hamandakuntz <hamanda.monteiro@hotmail.com>
2021-06-09 18:06:46 -0300

    início

--- src/components/PostShowPage/PostShowPage.js
+++ src/components/PostShowPage/PostShowPage.js
@@ -17,7 +17,7 @@ export default function PostShowPage() {
   useEffect(() => {
     setPost({
       id: 1,
-      title: 'Hello World',
+      title: 'Helloo World',
       coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
       contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
       content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...'
